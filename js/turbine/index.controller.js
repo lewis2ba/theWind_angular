4df845c185046331b5a41065548da89c325218b8
Brendan Lewis <lewis2ba@dukes.jmu.edu>
2016-01-15 10:20:43 -0500

    styling changed, create functionality, backend attached

--- js/turbine/index.controller.js
+++ js/turbine/index.controller.js
@@ -9,5 +9,17 @@
 
   function TurbineIndexControllerFunction(TurbineFactory){
     this.turbines = TurbineFactory.query()
+    var totalCapacity = 0;
+    this.turbines.$promise.then(function(turbines){
+      turbines.forEach(function(turbine){
+        // console.log(rated_capacity)
+        totalCapacity += turbine.rated_capacity
+
+      })
+      var usCapacity = 4093000000000
+      var offset = (totalCapacity/usCapacity)*100
+      console.log(totalCapacity)
+      $('#turbineStatistics').append("<h3>Users of the Wind are offsetting "+totalCapacity+" kilowatts of capacity with wind energy! Thats about " + offset + "% of the US' total installed capacity!</h3>")
+    })
   }
 }())
